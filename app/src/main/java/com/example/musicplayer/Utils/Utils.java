185f4b544bd6ed6ba596a93175f18da552f036f7
mahsashafiee <45947075+mahsashafiee@users.noreply.github.com>
2020-01-24 14:31:09 +0330

    splash screen added

--- app/src/main/java/com/example/musicplayer/Utils/Utils.java
+++ app/src/main/java/com/example/musicplayer/Utils/Utils.java
@@ -1,13 +1,27 @@
 package com.example.musicplayer.Utils;
 
+import android.app.Activity;
 import android.os.Build;
 import android.text.Html;
 import android.text.Spanned;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Utils {
     public static Spanned buildSpanned(String res) {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.N ?
                 Html.fromHtml(res, Html.FROM_HTML_MODE_LEGACY) :
                 Html.fromHtml(res);
+    }
+
+    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
+        Window win = activity.getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
     }
 }
--- app/src/main/java/com/example/musicplayer/controller/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/controller/MainActivity.java
@@ -2,9 +2,17 @@ package com.example.musicplayer.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.musicplayer.R;
+
+import static com.example.musicplayer.Utils.Utils.setWindowFlag;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +20,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View view = findViewById(R.id.parent_layout);
+
+        //make translucent statusBar on kitkat devices
+        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
+        }
+        if (Build.VERSION.SDK_INT >= 19) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+        //make fully Android Transparent Status bar
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
+        AnimationDrawable animDrawable = (AnimationDrawable) view.getBackground();
+        animDrawable.setEnterFadeDuration(10);
+        animDrawable.setExitFadeDuration(2000);
+        animDrawable.start();
+
     }
 }
